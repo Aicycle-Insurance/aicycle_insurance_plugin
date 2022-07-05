9956bc1855648ff14d3d00952697eb3085f5369f
Danh Quyen <quyenpd6841@seta-international.vn>
2022-07-05 10:42:02 +0700

    update constant

---
+++ lib/constants/car_directions_constant.dart
@@ -0,0 +1,31 @@
+enum CarDirections {
+  /// Mặt trái - trước xe
+  leftHead,
+
+  /// Mặt trái - sau xe
+  leftTail,
+
+  /// Mặt phải - trước xe
+  righteHead,
+
+  /// Mặt phải - sau xe
+  rightTail,
+
+  /// Mặt trước xe góc 45 độ trái
+  left45DegreeFrontOfCar,
+
+  /// Mặt trước xe góc thẳng
+  frontOfCar,
+
+  /// Mặt trước xe góc 45 độ phải
+  right45DegreeFrontOfCar,
+
+  /// Mặt sau xe góc 45 độ trái
+  left45DegreeBehindCar,
+
+  /// Mặt sau xe góc thẳng
+  behindCar,
+
+  /// Mặt sau xe góc 45 độ phải
+  right45DegreeBehindCar,
+}
